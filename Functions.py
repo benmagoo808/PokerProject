a8f71b811c8fdaac940743258563bbd3ce064125
Ben <benmagoo@gmail.com>
2017-06-25 14:10:23 -0700

    Finished function which detects a straight, although it doesn't return the high card yet. Those lines of code which are associated with that part of the function are commented out for now. Out of time today. This proved harder than expected.

--- Functions.py
+++ Functions.py
@@ -1,6 +1,8 @@
 from Classes import Deck
 import re
 from collections import Counter
+from operator import itemgetter
+from itertools import groupby
 
 
 
@@ -68,7 +70,53 @@ def detect_multiple():
         return False
 
 
+def detect_straight():
+    # Create dictionary linking each card to its ranking
+    rank_values = {'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7,
+                   '9': 8,'10': 9, 'Jack': 10,'Queen': 11, 'King': 12,
+                   'Ace': 13}
+    #reverse_values = reversed(rank_values)
+    hand_values = []
+    no_dup_values = []
+    runs = []
+    high_card_value = []
+    high_card = []
+    straight = False
+    # Iterate through total_cards and add the card ranks to hand_values
+    for i in range(len(total_cards[hand_count])):
+        card = total_cards[hand_count][i].split('-')
+        hand_values.append(rank_values.get(card[0]))
+    # Get rid of any duplicates
+    for value in hand_values:
+        if value not in no_dup_values:
+            no_dup_values.append(value)
+    # If any Aces, add the unlisted value 0 to account for low straight
+    if max(no_dup_values) == 13:
+        no_dup_values.append(0)
+    # Sort low to high
+    no_dup_values.sort()
+    # Create runs a list of lists with consecutive values together in a list
+    # and non consecutive values the only list item
+    for k, g in groupby(list(enumerate(no_dup_values)), lambda x: x[0]-x[1]):
+        runs.append(list(map(itemgetter(1), g)))
+    # See if any of the lists have 5 cards in them (a poker straight)
+    # If they do, then reverse the dictionary and .get the card to return
+    for i in range(len(runs)):
+        if len(runs[i]) > 4:
+            straight = True
+            #high_card_value.append(runs[i][-1])
+            #high_card = reverse_values.get(high_card_value[0])
+
+    if straight == True:
+        return str("Straight ") # + str(high_card[0]) + " high.")
+    else:
+        return False
+
+
+
 
 start_new_hand()
+print("\n" + str(total_cards))
 print(detect_multiple())
+print(detect_straight())
 
